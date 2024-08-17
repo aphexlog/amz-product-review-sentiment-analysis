5a5846ad2fee65cd49cef397b5cad4a505e11345
Aaron West <aphexlog@gmail.com>
2024-08-17 11:02:38 -0500

    adding .gitignore, pyright.json for managing venvs and added content to prep.py

--- prep.py
+++ prep.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import boto3
+from mypy_boto3_s3 import S3Client
+
+# S3 client
+client: S3Client = boto3.client('s3') # type: ignore
 
 def prep_reviews(file_obj):
     # Load the CSV file from the file object
@@ -11,8 +16,14 @@ def prep_reviews(file_obj):
     df = df[['Summary', 'Text'] + [col for col in df.columns if col not in ['Summary', 'Text']]]
 
     # Write the new CSV content to a new file
-    df.to_csv('Filtered_Reviews.csv', index=False)
+    df.to_csv('Filtered_Reviews2.csv', index=False)
 
 # Example usage
 # data = client.get_object(Bucket='elevator-robot-sagemaker', Key='Reviews.csv')
 # prep_reviews(data)
+#
+bucket = 'elevator-robot-sagemaker'
+src_key = 'Reviews.csv'
+
+data = client.get_object(Bucket=bucket, Key=src_key)
+prep_reviews(data)
